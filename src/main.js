a66d228f5861090997167b9bd3a212e15ad22782
王亚星 <funnycoderstar@gmail.com>
2017-04-24 18:58:53 +0800

    feat: 添加基础配置

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import App from './App';
+require('normalize.css');
+// import { Router, Route, Switch } from 'react-router';
 
 ReactDom.render(
     <App />,
