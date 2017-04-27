2a0a2de05aa8e91ca6a118624485dd36674f9ebc
王亚星 <funnycoderstar@gmail.com>
2017-04-28 00:02:02 +0800

    feat: 添加react-router-dom ,修改tab实现方式

--- .eslintrc.js
+++ .eslintrc.js
@@ -45,5 +45,7 @@ module.exports = {
         'react/no-array-index-key': 0,
         'jsx-a11y/img-has-alt': 0,
         'jsx-a11y/no-static-element-interactions': 0,
+        'react/prop-types': 0,
+        'linebreak-style': 0,
     }
 };
--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,66 @@
 import React, { Component } from 'react';
+
+import '../src/styles/hello.less';
 // import {
 //     BrowserRouter as Router,
 //     Route,
 // } from 'react-router-dom';
 
 // import TabBar from './components/tabBar';
-import TabBar from './components/tabItem';
+// import TabBar from './components/tabItem';
 // import hero from './components/hero/heroHead';
 // import raider from './components/raiders/raiders';
 // import recommend from './components/recommend/recommend';
 // import video from './components/video/video';
 
 // App can't be stateless component
+// class A extends Component {
+//     render() {
+//         return <div>A</div>;
+//     }
+// }
+// class B extends Component {
+//     render() {
+//         return <div>B</div>;
+//     }
+// }
+// class C extends Component {
+//     render() {
+//         return <div>C</div>;
+//     }
+// }
+
 export default class App extends Component {
-    // constructor(...args) {
-    //     super(...args);
-    //     this.state = {
-    //         count: 0,
-    //     };
-    // }
-    // handleClick = () => {
-    //     this.setState({ count: this.state.count + 1 });
-    // }
+    constructor(...args) {
+        super(...args);
+        this.state = {
+            selected: 0,
+        };
+    }
+    handleClick = (index) => {
+        this.setState({ selected: index });
+    }
     render() {
         return (
-            <div className="container" >
-                <TabBar>
-                    <div name="first">
-                        第一帧
+            <div className="tabBar">
+                {
+                    ['式神', '推荐', '视频', '攻略'].map((item, index) => (
+                        <div
+                            className="tabTitle"
+                            style={{ color: this.state.selected === index + 1 ? 'red' : 'black' }} onClick={this.handleClick.bind(this, index + 1)}
+                        >
+                            <i className="icon" />
+                            <p className="tabText" >{item}</p>
+                        </div>
+                    ))
+                }
+                {/* <Router>
+                    <div>
+                        <Route path="/1" component={A} />
+                        <Route path="/2" component={B} />
+                        <Route path="/3" component={C} />
                     </div>
-                    <div name="second">
-                        第二帧
-                    </div>
-                    <div name="third">
-                        第三帧
-                    </div>
-                </TabBar>
+                </Router>*/}
             </div >
         );
     }
