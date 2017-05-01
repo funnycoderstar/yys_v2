dd985966453beeb1f3f0b88ad9b258a97503a98b
王亚星 <funnycoderstar@gmail.com>
2017-05-01 14:01:45 +0800

    feat: 添加mock数据

--- src/main.js
+++ src/main.js
@@ -6,6 +6,12 @@ import App from './App';
 require('normalize.css');
 
 require('./config/match.js');
+
+// 使用mock.js拦截ajax请求
+if (process.env.NODE_ENV !== 'production') {
+    require('./mocks/index.js');
+}
+
 // import { Router, Route, Switch } from 'react-router';
 
 ReactDom.render(
---
+++ src/mocks/index.js
@@ -0,0 +1,5 @@
+import Mock from 'mockjs';
+// 咨询轮播图
+Mock.mock(/yys\/newsImg/, {
+
+});
