09ac9cbf44d48e29c3f170cb3ed7ca6d4dae3f09
王亚星 <funnycoderstar@gmail.com>
2017-05-04 23:32:41 +0800

    feat: 使用ant完成heroSwiper

--- src/components/hero/heroSwiper.jsx
+++ src/components/hero/heroSwiper.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import { Carousel } from 'antd';
 import '../../styles/heroSwiper.less';
 
 class heroSwiper extends Component {
     render() {
         return (
-            <div className="heroSwiper" />
+            <div className="heroSwiper">
+                <Carousel autoplay dots="false">
+                    <div><img src={require('../../assets/banner1.jpg')}alt="" /></div>
+                    <div><img src={require('../../assets/banner2.jpg')}alt="" /></div>
+                    <div><img src={require('../../assets/banner3.jpg')}alt="" /></div>
+                    <div><img src={require('../../assets/banner4.jpg')}alt="" /></div>
+                </Carousel>
+            </div>
         );
     }
 }
