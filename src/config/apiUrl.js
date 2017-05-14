83d917ed4e99b06c1cc1cc1bfb86d24e19dbecb8
王亚星 <funnycoderstar@gmail.com>
2017-05-15 00:27:49 +0800

    feat: 配置后端接口

---
+++ src/config/apiUrl.js
@@ -0,0 +1,7 @@
+const apiUrl = 'http://192.168.199.183:6600/';
+export default {
+    herosUrl: `${apiUrl}heros`, // 式神
+    heroVideoUrl: `${apiUrl}heroVideo`, // 式神视频
+    heroStrategyUrl: `${apiUrl}heroStrategy`, // 式神
+}
+
