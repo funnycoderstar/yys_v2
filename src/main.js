d51950af79aa195e00b16b43c68e4bc9e69622b2
王亚星 <funnycoderstar@gmail.com>
2017-05-17 15:32:05 +0800

    feat: 使用lib-flexible布局方案

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
-// import 'lib-flexible';
+import 'lib-flexible';
 import { createStore } from 'redux';
 import App from './App';
 import reducer from './redux/reducer.js';
 
 require('normalize.css');
-require('./config/match.js');
+// require('./config/match.js');
 
 // // 使用mock.js拦截ajax请求
 // // if (process.env.NODE_ENV !== 'production') {
